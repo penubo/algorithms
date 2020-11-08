21d5a3af2d76c7c6e485d9f7a80a3cefbdf50bdd
yongjoon <yongjoon@bearrobotics.ai>
2020-11-08 20:19:54 +0900

    [init] ts config for quicksort

---
+++ sorting/quick-sort/ts/dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const a = 10 || 20 || 30;
+const arr = [];
+arr.push(1);
+console.log(arr);
+//# sourceMappingURL=main.js.map
---
+++ sorting/quick-sort/ts/main.ts
@@ -0,0 +1,9 @@
+const a = 10 || 20 || 30
+
+type MyList = number[]
+
+const arr: MyList = []
+
+arr.push(1)
+
+console.log(arr)
