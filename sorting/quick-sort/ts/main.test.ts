5c19c4be0c9f1012568faa620f82c5f06a4909c6
yongjoon <yongjoon@bearrobotics.ai>
2020-11-13 00:44:17 +0900

    added quicksort

--- sorting/quick-sort/ts/main.test.ts
+++ sorting/quick-sort/ts/main.test.ts
@@ -1,15 +1,21 @@
-import quicksort from './main';
+import quicksort from './main'
 
 describe('quicksort unit test', () => {
   it('should sort 1 number', () => {
-    const arr = [1];
-    const sortedArr = quicksort(arr, 0, arr.length);
-    expect(sortedArr).toEqual([1]);
-  });
+    const arr = [1]
+    const sortedArr = quicksort(arr, 0, arr.length)
+    expect(sortedArr).toEqual([1])
+  })
 
   it('should sort 2 numbers', () => {
-    const arr = [2, 1];
-    const sortedArr = quicksort(arr, 0, arr.length);
-    expect(sortedArr).toEqual([1, 2]);
-  });
-});
+    const arr = [2, 1]
+    const sortedArr = quicksort(arr, 0, arr.length)
+    expect(sortedArr).toEqual([1, 2])
+  })
+
+  it('should sort more than 2 numbers', () => {
+    const arr = [5, 2, 3, 4, 1, 6]
+    const sortedArr = quicksort(arr, 0, arr.length)
+    expect(sortedArr).toEqual([1, 2, 3, 4, 5, 6])
+  })
+})
--- sorting/quick-sort/ts/main.ts
+++ sorting/quick-sort/ts/main.ts
@@ -1,7 +1,45 @@
-const quicksort = (arr: number[], start: number, end: number): number[] => {
-  if (arr.length === 1) return [1];
-  const newArr = [1, 2];
-  return newArr;
-};
+const swapArrElement = (arr: number[], a: number, b: number) => {
+  const tmp = arr[a]
+  arr[a] = arr[b]
+  arr[b] = tmp
+}
 
-export default quicksort;
+const getPartition = (arr: number[], start: number, end: number): number => {
+  const lastIndex: number = end - 1
+  let startIndex: number = start
+  let endIndex: number = lastIndex - 1
+
+  let pendingLeft: number | null = null
+  let pendingRight: number | null = null
+
+  while (startIndex < endIndex) {
+    if (arr[startIndex] > arr[lastIndex]) {
+      pendingLeft = startIndex
+    }
+    if (arr[endIndex] < arr[lastIndex]) {
+      pendingRight = endIndex
+    }
+    if (pendingLeft && pendingRight) {
+      swapArrElement(arr, pendingLeft, pendingRight)
+      pendingLeft = null
+      pendingRight = null
+    }
+    if (pendingLeft === null) startIndex += 1
+    if (pendingRight === null) endIndex -= 1
+  }
+  if (arr[endIndex] > arr[lastIndex]) swapArrElement(arr, endIndex, lastIndex)
+
+  return endIndex
+}
+
+const quicksort = (arr: number[], start: number, end: number): number[] => {
+  if (end - start <= 1) return arr
+
+  const partition = getPartition(arr, start, end)
+  quicksort(arr, start, partition + 1)
+  quicksort(arr, partition + 1, end)
+
+  return arr
+}
+
+export default quicksort
